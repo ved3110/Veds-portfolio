b88e821802f57496cc69ce9a13c35a2922239a72
ved3110 <137319887+ved3110@users.noreply.github.com>
2026-06-19 01:22:25 +0530

    updated personal info

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,13 @@
 export const personalData = {
-  name: "ABU SAID",
+  name: "VEDANT PATHAK",
   profile: '/profile.png',
-  designation: "Software Developer",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'abusaid7388@gmail.com',
-  phone: '+8801608797655',
-  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  designation: "Software Development Engineer in Test",
+  description: "My name is Vedant Pathak. I am a passionate Software Development Engineer in Test (SDET) with expertise in Selenium, Java, and API Testing. I thrive on building robust automation frameworks and am driven by a continuous desire to learn and deliver high-quality software solutions.",
+  email: 'vedantpathak02@gmail.com',
+  phone: '9623810813',
+  address: 'Hyderabad, India',
+  github: 'https://github.com/ved3110',
+  linkedIn: https://www.linkedin.com/in/vedant-pathak-qa/',
+  devUsername: "ved3110",
+  resume: "YOUR_GOOGLE_DRIVE_RESUME_LINK"
 }
